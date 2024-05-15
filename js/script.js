6d69e50f1a5756daa4fe8606d42b97539c8659ed
Rangga Nicholas <rsptr492@gmail.com>
2024-05-16 01:06:14 +0700

    membuat website dyraja olshop

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Toogle Class Active
+
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika humberger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// diluar side bar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
